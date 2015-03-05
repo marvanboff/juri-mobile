afb78e476cc7378832a95ea76b4e807004f388de
marvanboff <marvanboff@gmail.com>
2015-03-05 18:41:36 -0300

    Implementação do botão open close drawer.

--- JuriMobile/gen/br/unisinos/jurimobile/R.java
+++ JuriMobile/gen/br/unisinos/jurimobile/R.java
@@ -18,12 +18,14 @@ public final class R {
     public static final class color {
         public static final int accent=0x7f040000;
         public static final int background=0x7f040003;
+        public static final int background_drawer=0x7f040004;
         public static final int primary=0x7f040001;
         public static final int primary_dark=0x7f040002;
-        public static final int secundaryText=0x7f040004;
+        public static final int secundaryText=0x7f040005;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_drawer=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int drawer_layout=0x7f080000;
@@ -38,26 +40,33 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int drawer_close=0x7f050002;
+        public static final int drawer_open=0x7f050001;
+        public static final int meusProcessos=0x7f050003;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
+
     
  API 11 theme customizations can go here. 
 
         Base application theme for API 14+. This theme completely replaces
         AppBaseTheme from BOTH res/values/styles.xml and
         res/values-v11/styles.xml on API 14+ devices.
+
     
  API 14 theme customizations can go here. 
          */
--- JuriMobile/src/br/unisinos/jurimobile/controller/ProcessoListActivity.java
+++ JuriMobile/src/br/unisinos/jurimobile/controller/ProcessoListActivity.java
@@ -3,11 +3,17 @@ package br.unisinos.jurimobile.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import br.unisinos.jurimobile.R;
@@ -26,6 +32,11 @@ public class ProcessoListActivity extends Activity {
 	private DrawerLayout drawerLayout;
 	private String[] options;
 	private ListView drawerList;
+
+	private ActionBarDrawerToggle drawerToggle;
+    private ActionBarHelper mActionBar;
+
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +59,60 @@ public class ProcessoListActivity extends Activity {
 		drawerList = (ListView) findViewById(R.id.processos_drawer);
 		drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,options));
 		
-	}
+		
+	   mActionBar = createActionBarHelper();
+       mActionBar.init();
+        
+       drawerToggle = new ActionBarDrawerToggle(
+                this,               
+                drawerLayout,       
+                R.drawable.ic_drawer,
+                R.string.drawer_open,  
+                R.string.drawer_close   
+                ) {
+			public void onDrawerClosed(View view) {
+				super.onDrawerClosed(view);
+//				getSupportActionBar().setTitle("onDrawerClosed");
+//				supportInvalidateOptionsMenu();
+			}
 
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+//				getSupportActionBar().setTitle("onDrawerOpened");
+//				supportInvalidateOptionsMenu();
+			}
+        };
+        
+        drawerLayout.setDrawerListener(drawerToggle);
+		
+	}
+	
+	@Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /*
+         * The action bar home/up action should open or close the drawer.
+         * mDrawerToggle will take care of this.
+         */
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+	
 	public List<Processo> getMockProcessos() {
 		List<Processo> processos = new ArrayList<>();
 
@@ -126,5 +189,76 @@ public class ProcessoListActivity extends Activity {
 
 		return processos;
 	}
+	
+	/** ACTION BAR */
+	
+	/**
+     * Create a compatible helper that will manipulate the action bar if available.
+     */
+    private ActionBarHelper createActionBarHelper() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            return new ActionBarHelperICS();
+        } else {
+            return new ActionBarHelper();
+        }
+    }
 
+    /**
+     * Stub action bar helper; this does nothing.
+     */
+    private class ActionBarHelper {
+        public void init() {}
+        public void onDrawerClosed() {}
+        public void onDrawerOpened() {}
+        public void setTitle(CharSequence title) {}
+    }
+
+    /**
+     * Action bar helper for use on ICS and newer devices.
+     */
+    private class ActionBarHelperICS extends ActionBarHelper {
+        private final ActionBar mActionBar;
+        private CharSequence mDrawerTitle;
+        private CharSequence mTitle;
+
+        ActionBarHelperICS() {
+            mActionBar = getActionBar();
+        }
+
+        @Override
+        public void init() {
+            mActionBar.setDisplayHomeAsUpEnabled(true);
+            mActionBar.setHomeButtonEnabled(true);
+            mTitle = mDrawerTitle = getTitle();
+        }
+
+        /**
+         * When the drawer is closed we restore the action bar state reflecting
+         * the specific contents in view.
+         */
+        @Override
+        public void onDrawerClosed() {
+            super.onDrawerClosed();
+            mActionBar.setTitle(mTitle);
+        }
+
+        /**
+         * When the drawer is open we set the action bar to a generic title.
+         * The action bar should only contain data relevant at the top level of
+         * the nav hierarchy represented by the drawer, as the rest of your content
+         * will be dimmed down and non-interactive.
+         */
+        @Override
+        public void onDrawerOpened() {
+            super.onDrawerOpened();
+            mActionBar.setTitle(mDrawerTitle);
+        }
+
+        @Override
+        public void setTitle(CharSequence title) {
+            mTitle = title;
+        }
+    }
+	
+	
 }
